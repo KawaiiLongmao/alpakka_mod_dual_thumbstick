c462ba4e80badb31d1453c6a9375efac28c4b043
可爱龙猫 <11011608+KawaiiLongmao@user.noreply.gitee.com>
2025-10-31 13:25:57 +0800

    20251031

--- firmware_source/alpakka_mod_firmware_development/src/headers/self_test.h
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-only
-// Copyright (C) 2022, Input Labs Oy.
-
-#pragma once
-
-void self_test();
--- firmware_source/alpakka_mod_firmware_stable/src/headers/thanks.h
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-only
-// Copyright (C) 2022, Input Labs Oy.
-
-extern uint8_t thanks_len;
-extern uint8_t thanks_list[11][24];
